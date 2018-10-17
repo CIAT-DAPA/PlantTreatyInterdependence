62d43ef44daf30600d123f8d15466853f84a0e8c
stevensotelo <stevenbetancurt@hotmail.com>
2018-10-17 14:23:48 -0500

    Working in import data from faostat

---
+++ src/R/scripts/config.R
@@ -0,0 +1,86 @@
+library(RMySQL)
+
+##############################################
+####  00-GLOBAL VARIABLES
+##############################################
+
+setwd("G:/CIAT/Code/CWR/PlantTreatyInterdependence/src/R/")
+
+# Global variables
+conf.folder = "conf"
+conf.file = "conf_test.csv"
+inputs.folder = "inputs"
+process.folder = "process"
+
+conf.variables = read.csv(paste0(conf.folder,"/",conf.file ), header = T)
+
+
+# Database connection
+db_cnn <- dbConnect(MySQL(),user = as.character(conf.variables[which(conf.variables$name == "db_user"),"value"]),
+                    password = as.character(conf.variables[which(conf.variables$name == "db_password"),"value"]),
+                    host = as.character(conf.variables[which(conf.variables$name == "db_host"),"value"]),
+                    dbname=as.character(conf.variables[which(conf.variables$name == "db_name"),"value"]))
+
+#dbDisconnect(db_cnn)
+
+##############################################
+####  00-IMPORT GLOBAL PARAMETERS
+##############################################
+
+# This function saves a list of countries in the database
+# (string) f: Path of file to import into database
+conf.import.countries = function(f){
+  countries = read.csv(paste0(conf.folder, "/", f), header = T)
+  print(paste0("........Countries were loaded from file"))
+  
+  # Get list of countries from database
+  process.countries.query = dbSendQuery(db_cnn,paste0("select id,name,iso2,iso3 from countries"))
+  process.countries = fetch(process.countries.query, n=-1)
+  print(paste0("........Countries were loaded from database"))
+  
+  if(dim(process.countries)[1]>0){
+    countries = countries[!which(countries$iso2 %in% process.countries$iso2),]  
+  }
+  
+  
+  if(dim(countries)[1]>0){
+    dbWriteTable(db_cnn, value = countries, name = "countries", append = TRUE, row.names=F)
+  }
+  print(paste0("........Records were saved ", dim(countries)[1]))
+}
+
+# This function saves a list of crops in the database
+# (string) f: Path of file to crops into database
+conf.import.crops = function(f){
+  crops = read.csv(paste0(conf.folder, "/", f), header = T)
+  print(paste0("........Crops were loaded from file"))
+  
+  # Get list of crops in database
+  process.crops.query = dbSendQuery(db_cnn,paste0("select id,name from crops"))
+  process.crops = fetch(process.crops.query, n=-1)
+  print(paste0("........Crops were loaded"))
+  
+  if(dim(process.crops)[1]>0){
+    crops = crops[!which(crops$Item %in% process.crops$name),]
+  }
+  
+  crops = crops[order(crops$Item),] 
+  crops.new = unique(data.frame(name = crops$Item))
+  
+  
+  if(dim(crops.new)[1]>0){
+    dbWriteTable(db_cnn, value = crops.new, name = "crops", append = TRUE, row.names=F)
+  }
+  
+  print(paste0("........Records were saved ", dim(crops)[1]))
+}
+
+##############################################
+####  01-PROCESS
+##############################################
+
+conf.import.countries("countries.csv")
+
+conf.import.crops("crops.csv")
+
+
--- src/R/scripts/domains.R
+++ src/R/scripts/domains.R
@@ -56,15 +56,6 @@ inputs.download = function(i){
   
 }
 
-inputs.domains.query = dbSendQuery(db_cnn,"select id,name,source,url from domains")
-
-# Get list of domains available
-inputs.domain = fetch(inputs.domains.query, n=-1)
-
-# Download data
-lapply(1:nrow(inputs.raw),inputs.download)
-
-
 ##############################################
 ####  02-SET UP THE MAIN ENTITIES
 ##############################################
@@ -98,65 +89,7 @@ process.load.metrics = function(f){
   
 }
 
-# This function saves the records of countries from file
-# (string) f: File name
-process.load.countries = function(f){
-  tmp.source.domain = gsub(".csv","",unlist(strsplit(f, "-")))
-  tmp.measure = read.csv(paste0(inputs.folder,"/",f ), header = T)
-  print(paste0("........Parameters were loaded"))
-  
-  # Get list of countries
-  process.countries.query = dbSendQuery(db_cnn,paste0("select id,name,iso2 from countries"))
-  process.countries = fetch(process.countries.query, n=-1)
-  print(paste0("........Countries were loaded"))
-  
-  # Compare data from database with the new values
-  tmp.values = unique(tmp.measure[,c("Area.Code","Area")])
-  tmp.new.values = subset(tmp.values,!(Area %in% process.countries$name))
-  
-  if(dim(tmp.new.values)[1] > 0){
-    tmp.new.values = tmp.new.values[order(tmp.new.values$Area),]
-    tmp.new.values$iso2 = ""
-    names(tmp.new.values)=c("id","name","iso2")
-    tmp.new.values = tmp.new.values[,c("name","iso2")]
-    print(paste0("........The data was cleaned"))
-    
-    dbWriteTable(db_cnn, value = tmp.new.values, name = "countries", append = TRUE, row.names=F)
-    print(paste0("........Records were saved ", dim(tmp.new.values)[1]))
-  }
-}
 
-# This function saves the records of crops from file
-# (string) f: File name
-process.load.crops = function(f){
-  tmp.source.domain = gsub(".csv","",unlist(strsplit(f, "-")))
-  tmp.measure = read.csv(paste0(inputs.folder,"/",f ), header = T)
-  print(paste0("........Parameters were loaded"))
-  
-  # Get list of crops in database
-  process.crops.query = dbSendQuery(db_cnn,paste0("select id,name from crops"))
-  process.crops = fetch(process.crops.query, n=-1)
-  print(paste0("........Crops were loaded"))
-  
-  # Compare data from database with the new values
-  tmp.values = unique(tmp.measure[,c("Item.Code","Item")])
-  tmp.new.values = subset(tmp.values,!(Item %in% process.crops$name))
-  
-  if(dim(tmp.new.values)[1] > 0){
-    tmp.new.values =  data.frame(name = tmp.new.values[order(tmp.new.values$Item),c("Item")])
-    
-    # Get usable crops from source
-    tmp.source = read.csv(paste0(conf.folder,"/",tmp.source.domain[1], ".csv" ), header = T)
-    tmp.merge = merge(x=tmp.new.values, y=tmp.source, by.x="name", by.y="Item")
-    tmp.merge = tmp.merge[tmp.merge$Useable == "Y",]
-    tmp.column = ifelse(tmp.merge$Useable == "Y", 1, 0)
-    tmp.final = unique(data.frame(name = tmp.merge$name, usable = tmp.column))
-    print(paste0("........The data was cleaned"))
-    
-    dbWriteTable(db_cnn, value = tmp.final, name = "crops", append = TRUE, row.names=F)
-    print(paste0("........Records were saved ", dim(tmp.final)[1]))
-  }
-}
 
 # This function saves the records of measure from file
 # (string) f: File name
@@ -164,59 +97,48 @@ process.load.measure = function(f){
   tmp.source.domain = gsub(".csv","",unlist(strsplit(f, "-")))
   tmp.domain = inputs.domain[inputs.domain$name == as.character(tmp.source.domain[2]),]
   tmp.measure = read.csv(paste0(inputs.folder,"/",f ), header = T)
-  print(paste0("........Parameters were loaded"))
+  print(paste0("........Measures were loaded"))
   
   # Get list of countries in database
-  process.countries.query = dbSendQuery(db_cnn,paste0("select id as id_country,name from countries"))
-  process.countries = fetch(process.countries.query, n=-1)
+  tmp.countries = read.csv(paste0(conf.folder,"/",tmp.source.domain[1],"-countries.csv" ), header = T)
   print(paste0("........Countries were loaded"))
-  
-  # Get list of domains metrics by domain
-  process.metrics.query = dbSendQuery(db_cnn,paste0("select id as id_metric,name from metrics where id_domain = ",as.character(tmp.domain$id[1])))
-  process.metrics = fetch(process.metrics.query, n=-1)
-  print(paste0("........Metrics were loaded"))
+  # Merge with countries
+  tmp.measure = merge(x=tmp.measure, y=tmp.countries, by.x="Area", by.y=tmp.source.domain[1], all.x = F, all.y = F)
+  names(tmp.measure)[length(names(tmp.measure))]  = "id_country"
+  print(paste0("........Countries were merged"))
   
   # Get list of crops in database
-  process.crops.query = dbSendQuery(db_cnn,paste0("select id as id_crop,name from crops"))
-  process.crops = fetch(process.crops.query, n=-1)
+  tmp.crops = read.csv(paste0(conf.folder,"/",tmp.source.domain[1],"-crops.csv" ), header = T)
   print(paste0("........Crops were loaded"))
-  
-  # Get usable crops from source
-  tmp.source = read.csv(paste0(conf.folder,"/",tmp.source.domain[1], ".csv" ), header = T)
-  tmp.source = merge(x=tmp.source,y=process.crops,by.x="Item",by.y="name")
-  names(tmp.source)[length(names(tmp.source))]  = "id_crop_original"
-  tmp.source = merge(x=tmp.source,y=process.crops,by.x="Item_cleaned",by.y="name")
-  names(tmp.source)[length(names(tmp.source))]  = "id_crop_final"
-  print(paste0("........Crops were fixed"))
-  
-  
-  # Columns not usable are removed
-  tmp.merge = select(tmp.measure, -ends_with("F"))
-  tmp.merge = select(tmp.merge, -ends_with("Code"))
-  names(tmp.merge) = gsub("Y","",names(tmp.merge))
-  tmp.years = c(5,dim(tmp.merge)[2])
-  tmp.tmp = tmp.merge
-  
-  # Merge with countries
-  tmp.tmp = merge(x=tmp.tmp, y=process.countries, by.x="Area", by.y="name")
-  # Merge with metrics
-  tmp.tmp = merge(x=tmp.tmp, y=process.metrics, by.x="Element", by.y="name")
   # Merge with crops
-  tmp.tmp = merge(x=tmp.tmp, y=tmp.source, by.x="Item", by.y="Item")
-  print(paste0("........Measures were merged"))
+  tmp.measure = merge(x=tmp.measure, y=tmp.crops, by.x="Item", by.y=tmp.source.domain[1], all.x = F, all.y = F)
+  names(tmp.measure)[length(names(tmp.measure))]  = "id_crop"
+  print(paste0("........Crops were merged"))
   
-  # Filter useable
-  tmp.tmp = tmp.tmp[tmp.tmp$Useable == "Y",]
-  print(paste0("........Measures were filtered"))
+  # Get list of domains metrics by domain
+  tmp.metrics.query = dbSendQuery(db_cnn,paste0("select id,name from metrics where id_domain = ",as.character(tmp.domain$id[1])))
+  tmp.metrics = fetch(tmp.metrics.query, n=-1)
+  print(paste0("........Metrics were loaded"))
+  # Merge with metrics
+  tmp.measure = merge(x=tmp.measure, y=tmp.metrics, by.x="Element", by.y="name", all.x = F, all.y = F)
+  names(tmp.measure)[length(names(tmp.measure))]  = "id_metric"
+  print(paste0("........Metrics were merged"))
+  
+  # Fixing the fields
+  tmp.measure = select(tmp.measure, -ends_with("F"))
+  tmp.measure = select(tmp.measure, -ends_with("Code"))
+  tmp.measure = select(tmp.measure, -starts_with("name"))
+  names(tmp.measure) = gsub("Y","",names(tmp.measure))
+  
+  tmp.years = dim(tmp.measure)[2]-3
   
   # Create records by every year
-  tmp.final = do.call("rbind",lapply((tmp.years[2]-3):tmp.years[2],function(y){
-    tmp.value = data.frame(id_metric=tmp.tmp$id_metric,
-                           id_country=tmp.tmp$id_country,
-                           id_crop_original=tmp.tmp$id_crop_original,
-                           id_crop_final=tmp.tmp$id_crop_final,
-                           year=as.integer(names(tmp.tmp)[y]),
-                           value=tmp.tmp[,y])
+  tmp.final = do.call("rbind",lapply((tmp.years-3):tmp.years,function(y){
+    tmp.value = data.frame(id_metric=tmp.measure$id_metric,
+                           id_country=tmp.measure$id_country,
+                           id_crop=tmp.measure$id_crop,
+                           year=as.integer(names(tmp.measure)[y]),
+                           value=tmp.measure[,y])
     tmp.value[is.na(tmp.value)] = 0
     return (tmp.value)
   }))
@@ -226,23 +148,48 @@ process.load.measure = function(f){
   print(paste0("........Records were saved ", dim(tmp.final)[1]))
 }
 
+process.load.countries = function(f){
+  tmp.source.domain = gsub(".csv","",unlist(strsplit(f, "-")))
+  tmp.domain = inputs.domain[inputs.domain$name == as.character(tmp.source.domain[2]),]
+  tmp.measure = read.csv(paste0(inputs.folder,"/",f ), header = T)
+  print(paste0("........Measures were loaded"))
+  
+  tmp.countries = tmp.measure[,c("Area.Code","Area")]
+  tmp.countries = unique(tmp.countries)
+  write.csv(tmp.countries, paste0(tmp.source.domain[1],"-",tmp.source.domain[2],"-countries.csv"), row.names = F )
+  
+}
+
+
+
 # This function saves all data into database
 # (string) f: Name of file
 process.load = function(f){
   print(paste0("Star ", f))
-  print(paste0("....Metrics ", f))
-  process.load.metrics(f)
+  #print(paste0("....Metrics ", f))
+  #process.load.metrics(f)
   print(paste0("....Countries ", f))
   process.load.countries(f)
-  print(paste0("....Crops ", f))
-  process.load.crops(f)
-  print(paste0("....Measures ", f))
-  process.load.measure(f)
+  #print(paste0("....Measures ", f))
+  #process.load.measure(f)
 }
 
+##############################################
+####  03-PROCESS
+##############################################
+
+inputs.domains.query = dbSendQuery(db_cnn,"select id,name,source,url from domains")
+# Get list of domains available
+inputs.domain = fetch(inputs.domains.query, n=-1)
+
+# Download data
+lapply(1:nrow(inputs.raw),inputs.download)
+
 p = list.files(inputs.folder)
-p= c(p[4],p[5])
+#p= c(p[4],p[5])
 
 lapply(p,process.load)
 
-
+#tmp.c = read.csv("C:/Users/hsotelo/Desktop/domains.csv", header = T)
+#tmp.co = unique(tmp.c$crop_name)
+#write.csv(x = tmp.co,file = "C:/Users/hsotelo/Desktop/faostat-crops.csv" , row.names = F)
