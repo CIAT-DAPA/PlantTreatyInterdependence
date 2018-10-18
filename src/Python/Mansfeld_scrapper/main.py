5e0e8c1182ccd171be0c4ff5fa15bf3225b26856
Daniel Amariles <daniel.amariles88@gmail.com>
2018-10-18 10:56:02 -0500

    scrapper successfully implemented

--- src/Python/Mansfeld_scrapper/main.py
+++ src/Python/Mansfeld_scrapper/main.py
@@ -1,15 +1,16 @@
-
 import os
 import csv
 import mechanicalsoup
 from tqdm import tqdm
 from bs4 import BeautifulSoup
-import random
+import requests
 
 ### PARAMETROS
 
-userAgent= 'UOCBot/0.1: Mansfel scrapper'
+userAgent = 'UOCBot/0.1: Mansfel scrapper'
 terms_dict = {}
+
+
 # diccionario de cabecera y campos
 
 
@@ -22,58 +23,59 @@ def cleanValue(value):
     return value
 
 
-
 def getSpecies(desc, url):
     # crear la lista de convocatorias
     callsList = []
 
-
-    last =144
-    last=1
+    last = 144
+    last = 1
     progress_bar = tqdm(total=last, desc=desc)
 
     ### INICIAR LA NAVEGACIÖN
 
+    for use in range(1, last + 1):
 
-    for use in range(1, last+1):
-        browser = mechanicalsoup.StatefulBrowser(user_agent=userAgent)
-        browser.open(url)  # se abre la URL
+        s = requests.Session()
+        s.headers = {"User-Agent": "Mozilla/5.0"}
+        res = s.get(url)
+        soup = BeautifulSoup(res.text, "lxml")
 
+        arg_names = []
+        for name in soup.select("[name='p_arg_names']"):
+            arg_names.append(name['value'])
 
-        form = browser.select_form("#wwvFlowForm")
-        browser["P8_NUTZID"] = 35
-        browser["P8_TXTSEARCH"] = ""
+        salt = soup.select_one("[id='pSalt']")['value']
+        protected = soup.select_one("[id='pPageItemsProtected']")['value']
 
+        p_json = str(
+            '{"salt": "' + salt + '","pageItems":{"itemsToSubmit":[{"n":"P8_NUTZID","v":["35"]},{"n":"P8_TXTSEARCH","v":""}],"protected":"' + protected + '","rowVersion":""}}')
 
+        values = {
+            'p_flow_id': soup.select_one("[name='p_flow_id']")['value'],
+            'p_flow_step_id': soup.select_one("[name='p_flow_step_id']")['value'],
+            'p_instance': soup.select_one("[name='p_instance']")['value'],
+            'p_page_submission_id': soup.select_one("[name='p_page_submission_id']")['value'],
+            'p_request': "Go",
+            'p_reload_on_submit': "A",
+            'p_json': p_json
+        }
 
-        #browser.get_current_form().set("p_json", '{"salt":"255743010846329908763911115271411862552","pageItems":{"itemsToSubmit":[{"n":"P8_NUTZID","v":["35"]},{"n":"P8_TXTSEARCH","v":""}],"protected":"yRVqUGCru5r-KGko4a2HEw","rowVersion":""}}', True)
+        s.headers.update({'Referer': url})
+        response = s.post("http://mansfeld.ipk-gatersleben.de/apex/wwv_flow.accept", data=values)
+        print(response.text)
 
-        browser.get_current_form().set("p_flow_id", "185", True)
-        browser.get_current_form().set("p_flow_step_id", "8", True)
-        browser.get_current_form().set("p_instance", str(30491967542815+random.randint(1,101)), True)
-        #browser.get_current_form().set("p_page_submission_id", "255743010846329908763911115271411862552", True)
-        browser.get_current_form().set("p_request", "Go", True)
-        browser.get_current_form().set("p_reload_on_submit", "A", True)
+        soup2 = BeautifulSoup(response.text, "html.parser")
+        rows = soup2.findAll("a")
 
-
-        #form.choose_submit(submit)
-
-        response = browser.submit_selected()
-
-        soup = BeautifulSoup(response.text, "html.parser")
-        rows = soup.findAll("a")
-        print(browser.get_current_page())
-
-        # cerrar el navegador
-        browser.close()
         # actualizar la barra de progreso
         progress_bar.update(1)
         # añadiendo delay
         # time.sleep(0.1)
 
-    # cerrar barra de progreso
-    progress_bar.close()
+        # cerrar barra de progreso
+        progress_bar.close()
     return callsList
+
 
 def writeCSV(filename, callsList):
     # PARAMETROS ESCRITURA DEL ARCHIVO DE SALIDA
@@ -99,8 +101,6 @@ def writeCSV(filename, callsList):
             writer.writerow(call)
 
 
-
-
 ##################################################################
 
 # URL de la página del ICETEX a consultar las becas
@@ -110,7 +110,4 @@ url1 = "http://mansfeld.ipk-gatersleben.de/apex/f?p=185:8:"
 callsList1 = getSpecies(desc=fileCSV1, url=url1)
 writeCSV(fileCSV1, callsList1)
 
-
-
 ##################################################################
-
