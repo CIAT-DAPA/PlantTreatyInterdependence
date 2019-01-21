a8d7d768a806ee6ded3afe832b8e62fa369b2d80
Daniel Amariles <daniel.amariles88@gmail.com>
2019-01-21 14:26:34 -0500

    query by country

--- src/R/scripts/counts/GBIF_query.R
+++ src/R/scripts/counts/GBIF_query.R
@@ -17,75 +17,127 @@ if(!require("utils")){
   library("utils")
 }
 
-getNubKey <- function(.x, API_sp, rank, limit){
-  .x <- gsub(" ", "%20", .x)
-  query <- paste0(API_sp,"?q=",.x,"&rank=",rank,"&limit=",limit)
-  #query < - URLencode(query)
-  print(query)
-  nubKey <- fromJSON(query)$results$nubKey
-  
-  if(is.null(nubKey)){nubKey <- NA}
-  
-  return(nubKey)}
-
-getGenusCount <- function(.x, API_occ,  limit){
-  if(is.na(.x)){count = 0}else{
-  
-  query <- paste0(API_occ,"?genusKey=",.x,"&limit=",limit)
-
-  count <- fromJSON(query)$count }
-  
-  return(count)}
-
-getSpeciesCount <- function(.x, API_occ,  limit){
-  if(is.na(.x)){count = 0}else{
-    
-    query <- paste0(API_occ,"?speciesKey=",.x,"&limit=",limit)
-
-    count <- fromJSON(query)$count }
-  
-  return(count)}
-
+if(!require("countrycode")){
+  install.packages("countrycode")
+  library("countrycode")
+}
 
 
 path <- "D:/ToBackup/code/planttreaty/PlantTreatyInterdependence/src/R/scripts/counts/"
 API_sp  <- "http://api.gbif.org/v1/species/search"
 API_occ <- "http://api.gbif.org/v1/occurrence/search"
-limit = 1
+
+
+
+getNubKey <- function(.x, rank){
+  .x <- gsub(" ", "%20", .x) #TODO correctly enconde chars
+  
+  query <- paste0(API_sp,"?q=",.x,"&rank=",rank,"&limit=1")
+  nubKey <- fromJSON(query)$results$nubKey
+  
+  if(is.null(nubKey)){nubKey <- NA}
+  
+  return(nubKey)
+}
+
+getTaxaCount <- function(.x,rank){
+  
+  if(is.na(.x)){count = 0}else{
+  
+    query <- paste0(API_occ,"?",rank,"Key=",.x,"&limit=1")
+    count <- fromJSON(query)$count 
+    
+    }
+  
+  return(count)
+}
+
+
+getTaxaCountryCount<- function(.x,rank,country){
+
+  if(is.na(.x)||is.na(country)){count = 0}else{
+    
+    query <- paste0(API_occ,"?",rank,"Key=",.x,"&country=",country,"&limit=1")
+    count <- fromJSON(query)$count 
+
+    }
+
+  return(count)
+}
+
+# bind data frames
+u_data_frame <- function(x, y){
+  cbind(x, y)
+}
+
+
+# countries
+
+countries = na.omit(codelist$iso2c)
+countries = as.data.frame(countries)
+
+
+
+runTaxa<- function(rank){
+      
+    taxa <-read.csv(paste0(path,rank,"_raw.csv"), header=TRUE, sep=",")
+    colnames(taxa) <- c("taxa")
+    
+    # taxa <- taxa[1:3,]
+    
+
+    # getting taxa key
+    
+    taxa <- taxa %>% 
+      as_tibble() %>% 
+      mutate(nubKey = purrr::map(.x = taxa , .f = getNubKey, rank = rank) ) %>% 
+      unnest 
+    
+    # getting taxa count
+    
+    taxa_c = taxa
+    
+    taxa_c <- taxa_c %>% 
+      as_tibble() %>% 
+      mutate(count = purrr::map(.x = nubKey , .f = getTaxaCount, rank = rank) ) %>% 
+      unnest
+    
+    write.csv(taxa_c,file = paste0(path,"/GBIF_",rank,".csv"))
+    
+    
+    # species by country
+    
+    taxa_country <- taxa %>%
+      mutate(id = 1:nrow(.)) %>%
+      group_by(id) %>%
+      nest() %>%
+      mutate(countries = purrr::map2(.x = data, .y = countries, .f = u_data_frame)) %>%
+      unnest(countries) %>%
+      dplyr::select(-id) %>%
+      rename(country = y)
+    
+    taxa_country_c =taxa_country
+    
+    
+    taxa_country_c <- taxa_country_c %>% 
+      as_tibble() %>% 
+      mutate(count = purrr::map2(.x = nubKey , .y = country,  .f = getTaxaCountryCount, rank=rank) ) %>% 
+      unnest
+    
+    
+    write.csv(taxa_country_c,file = paste0(path,"/GBIF_",rank,"_country.csv"))
+
+}
+
+
+runTaxa("species")
+
+runTaxa("genus")
 
 
 
 
 
-
-
-
-species <-read.csv(paste0(path,"species_raw.csv"), header=FALSE, sep=",")
-
-species <- species %>% 
-  as_tibble() %>% 
-  # .[1:2,] %>% 
-  mutate(nubKey = purrr::map(.x = V1 , .f = getNubKey, API_sp = API_sp, rank = "SPECIES", limit = limit) ) %>% 
-  unnest %>% 
-  mutate(count = purrr::map(.x = nubKey , .f = getSpeciesCount, API_occ = API_occ, limit = limit) ) %>% 
-  unnest
-
-write.csv(species,file = paste0(path,"/GBIF_species.csv"))
-
-
-
-
-genus <-read.csv(paste0(path,"genus_raw.csv"), header=FALSE, sep=",")
-
-genus <- genus %>% 
-  as_tibble() %>% 
-  # .[1:2,] %>% 
-  mutate(nubKey = purrr::map(.x = V1 , .f = getNubKey, API_sp = API_sp, rank = "GENUS", limit = limit) ) %>% 
-  unnest %>% 
-  mutate(count = purrr::map(.x = nubKey , .f = getGenusCount, API_occ = API_occ, limit = limit) ) %>% 
-  unnest
-
-write.csv(genus,file = paste0(path,"/GBIF_genus.csv"))
 
 
 
