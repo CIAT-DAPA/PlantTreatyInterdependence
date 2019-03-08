6fdb08a732737b09c87d8ae9c748223f642ce8a6
Daniel Amariles <daniel.amariles88@gmail.com>
2019-03-08 13:06:35 -0500

    queries by institution country

--- src/SQL/GENESYS_SELECT_crop_by country.sql
+++ src/SQL/GENESYS_SELECT_crop_by country.sql
@@ -11,36 +11,86 @@ from CIAT_crop_taxon ct, genesys_2018.taxonomy2 t
 -- 22 seconds
 
 
--- subspecies counts by country
-CREATE table `CIAT_subspecies_counts` AS (select t.taxonName as subspecies ,  REPLACE(REPLACE(REPLACE(IFNULL(a.origCty , ''), '31', ''), '32', ''), '33', '')  as country, count(*) count
+-- subspecies counts by origin country
+CREATE table `CIAT_subspecies_origin_counts` AS (select t.taxonName as subspecies ,  REPLACE(REPLACE(REPLACE(IFNULL(a.origCty , ''), '31', ''), '32', ''), '33', '')  as country, count(*) count
 	from  taxonomy2 t
-	left join accession a on a.taxonomyId2 = t.id
+	inner join accession a on a.taxonomyId2 = t.id
 	where
 		(taxonName in  (select subspecies from CIAT_crop_species_subspecies group by subspecies)
         )
-	group by taxonName, a.origCty);
+	group by taxonName, country);
 -- Query OK, 63933 rows affected (4 min 51.29 sec)
 
-
--- genus counts by country
-CREATE table `CIAT_genus_counts` AS (select t.genus as genus ,  REPLACE(REPLACE(REPLACE(IFNULL(a.origCty , ''), '31', ''), '32', ''), '33', '')  as country, count(*) count
+-- genus counts by origin country
+CREATE table `CIAT_genus_origin_counts` AS (select t.genus as genus ,  REPLACE(REPLACE(REPLACE(IFNULL(a.origCty , ''), '31', ''), '32', ''), '33', '')  as country, count(*) count
 	from  taxonomy2 t
-	left join accession a on a.taxonomyId2 = t.id
+	inner join accession a on a.taxonomyId2 = t.id
 	where
 		(t.genus in  (select taxon from CIAT_crop_taxon where rank="genus" group by taxon)
         )
-	group by t.genus, a.origCty);
-    
+	group by t.genus, country);
 -- Query OK, 13501 rows affected (25.30 sec)
+    
 
+
+-- subspecies counts by institution country
+CREATE table `CIAT_subspecies_institutions_counts` AS (select t.taxonName as subspecies ,  SUBSTRING(instCode, 1, 3)  as country, count(*) count
+	from  taxonomy2 t
+	inner join accession a on a.taxonomyId2 = t.id
+	where
+		(taxonName in  (select subspecies from CIAT_crop_species_subspecies group by subspecies)
+        )
+	group by taxonName, country);
+    
+-- genus counts by origin country
+CREATE table `CIAT_genus_institutions_counts` AS (select t.genus as genus ,  SUBSTRING(instCode, 1, 3)  as country, count(*) count
+	from  taxonomy2 t
+	inner join accession a on a.taxonomyId2 = t.id
+	where
+		(t.genus in  (select taxon from CIAT_crop_taxon where rank="genus" group by taxon)
+        )
+	group by t.genus, country);
+
+
+
+
+-- counts of crop as species
+select c.crop, sum(s.count) as count
+from CIAT_crop_species_subspecies c
+inner join CIAT_subspecies_origin_counts s on s.subspecies=c.subspecies
+group by crop;
+
+
+-- counts of crop as genus
+select c.crop, sum(g.count) as count
+from CIAT_crop_taxon c
+inner join CIAT_genus_origin_counts g on g.genus=c.taxon
+where c.rank = "genus"
+group by crop;
+
+-- counts of crop by species by origin country
 select c.crop, c.species, s.country, sum(s.count) as count
 from CIAT_crop_species_subspecies c
-left join CIAT_subspecies_counts s on s.subspecies=c.subspecies
-group by crop, c.species, country;
+inner join CIAT_subspecies_origin_counts s on s.subspecies=c.subspecies
+group by crop, c.species, country ;
 
+-- counts of crop by genus by origin country
 select c.crop, g.genus, g.country, sum(g.count) as count
 from CIAT_crop_taxon c
-left join CIAT_genus_counts g on g.genus=c.taxon
+inner join CIAT_genus_origin_counts g on g.genus=c.taxon
+where c.rank = "genus"
+group by crop, g.genus, country;
+
+-- counts of crop by species by institution country
+select c.crop, c.species, s.country, sum(s.count) as count
+from CIAT_crop_species_subspecies c
+inner join CIAT_subspecies_institutions_counts s on s.subspecies=c.subspecies
+group by crop, c.species, country;
+
+-- counts of genus by species by institution country
+select c.crop, g.genus, g.country, sum(g.count) as count
+from CIAT_crop_taxon c
+inner join CIAT_genus_institutions_counts g on g.genus=c.taxon
 where c.rank = "genus"
 group by crop, g.genus, country;
 
