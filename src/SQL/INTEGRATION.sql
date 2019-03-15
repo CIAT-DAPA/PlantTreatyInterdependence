dcb051d8731047b5c3a9b5927bfa6c4b76b5bee2
Daniel Amariles <daniel.amariles88@gmail.com>
2019-03-15 14:22:09 -0500

    integration species as first two words

--- src/SQL/INTEGRATION.sql
+++ src/SQL/INTEGRATION.sql
@@ -3,8 +3,12 @@ use genesys_2018;
 select * from accession order by rand() limit 100;
 select * from taxonomy2 order by rand() limit 10;
 select * from WIEWS order by rand() limit 100;
-select * from sgsv order by rand() limit 100;
+select * from SGSV order by rand() limit 100;
 
+
+-- Tr. Triticum  
+-- S. Solanum
+-- O. Oryza
 
 create table GMERGE as (select 	a.acceNumb as original_id, 
 										LOWER(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(acceNumb, ';', ''), '_', ''), '\'', ''), '\"', ''), '@', ''), ':', ''), '.', ''), '-', ''), ' ', ''))  as id,
@@ -25,8 +29,8 @@ create table GMERGE as (select 	a.acceNumb as original_id,
                                         SUBSTRING_INDEX(`Taxon`,' ',1) as genus,
                                         SUBSTRING_INDEX(`Taxon`,' ',2) as species
                                         from WIEWS
-                                        where `Source of information` != "'GENESYS (https://www.genesys-pgr.org)'"
-                                        and `Source of information` != "'EURISCO (http://eurisco.ipk-gatersleben.de)'"
+                                        where `Source of information` like "%GENESYS%"
+                                        and `Source of information` like "%EURISCO%"
                                         )
 								union
 								(select accession_number as original_id, 
@@ -35,8 +39,8 @@ create table GMERGE as (select 	a.acceNumb as original_id,
                                         institute_code as institution,
                                         country_of_collection_or_source as country,
                                         genus as genus,
-                                        species
-                                        from sgsv);
+                                        SUBSTRING_INDEX(species,' ',2) as species
+                                        from SGSV);
 
 
 
@@ -45,18 +49,22 @@ select count(*) as total, COUNT( DISTINCT id, genus ) as uniques
 from GMERGE;
 
 -- # total, uniques
--- '9576633', '4818718'
--- 9649133, 5494164 con genus
-
+-- '5181751', '4332803'
 
 
 select *
 from GMERGE
 where id="19567";
 
-select *
+select * from SGSV
+where species like "Beta vulgaris%";
+
+
+select species, source, count(*)
 from GMERGE
-where species like "%.%";
+where species not like "%sp." and species not like "%spp." and species not like "%hybr." 
+and species like "%.%"
+group by species, source;
 
 create table GMERGE_duplicates as select * from (
 	select id, genus, count(*)  total
