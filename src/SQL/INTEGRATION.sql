0931da1be74f92867cc927d8eaf87a5f39902a75
Daniel Amariles <daniel.amariles88@gmail.com>
2019-03-15 14:49:09 -0500

    updates to fix species

--- src/SQL/INTEGRATION.sql
+++ src/SQL/INTEGRATION.sql
@@ -15,7 +15,7 @@ create table GMERGE as (select 	a.acceNumb as original_id,
 										"GENESYS" as source,
                                         instCode as institution,
                                         origCty as country,
-                                        t.genus as genus,
+                                        REPLACE(t.genus,' ','') as genus,
                                         SUBSTRING_INDEX(t.taxonName ,' ',2) as species
                                         from accession a
                                         left join taxonomy2 t on a.taxonomyId2 = t.id
@@ -42,8 +42,25 @@ create table GMERGE as (select 	a.acceNumb as original_id,
                                         SUBSTRING_INDEX(species,' ',2) as species
                                         from SGSV);
 
+UPDATE GMERGE
+SET genus= "Zea" , species = "Zea mays"
+WHERE genus = "Zea.mays";
+
+UPDATE GMERGE
+SET species = "Zea mays"
+WHERE species = "Zea z.mays";
+
+UPDATE GMERGE
+SET species = REPLACE(species, "O.", "Oryza"), genus= "Oryza"
+WHERE genus="O." and species like "O.%";
+
+UPDATE GMERGE
+SET species = REPLACE(species, "S.", "Solanum"), genus= "Solanum"
+WHERE genus="S." and species like "S.%";
 
 
+
+-- ############# control queries
 
 select count(*) as total, COUNT( DISTINCT id, genus ) as uniques
 from GMERGE;
@@ -65,6 +82,37 @@ from GMERGE
 where species not like "%sp." and species not like "%spp." and species not like "%hybr." 
 and species like "%.%"
 group by species, source;
+
+select genus, source, count(*)
+from GMERGE
+where species like "% %"
+group by genus, source;
+
+select genus, species, source, count(*)
+from GMERGE
+where genus like "%mays"
+group by genus, species, source;
+
+select genus, species, count(*)
+from GMERGE
+where species like "%Z.%"
+group by genus, species;
+
+select genus, species, count(*)
+from GMERGE
+where species like "%O.%"
+group by genus, species;
+
+select genus, species, count(*)
+from GMERGE
+where species like "%S.%"
+group by genus, species;
+
+
+
+
+
+
 
 create table GMERGE_duplicates as select * from (
 	select id, genus, count(*)  total
