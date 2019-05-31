a499acf51795ae898c01dca6e01b08de7be0ea74
Daniel Amariles <daniel.amariles88@gmail.com>
2019-05-31 14:49:55 -0500

    integration with gbif 2019

---
+++ src/SQL/USDA.sql
@@ -0,0 +1,23 @@
+use genesys_2018;
+
+select c.crop, sum(u.`count samples distributed`) from CIAT_crop_taxon c
+left join USDA_demand u on c.taxon = u.Genus
+where c.rank="genus"
+group by c.crop;
+
+select c.crop, sum(u.`count samples distributed`) from CIAT_crop_taxon c
+left join USDA_demand u on c.taxon = u.Species
+where c.rank="species"
+group by c.crop;
+
+
+select c.crop, u.country_code, u.year, sum(u.`count samples distributed`)  as count
+from CIAT_crop_taxon c 
+left join USDA_demand u on c.taxon = u.Genus
+where c.rank="genus"
+group by c.crop, u.country_code, u.year;
+
+select c.crop, u.country_code, u.year, sum(u.`count samples distributed`) from CIAT_crop_taxon c
+left join USDA_demand u on c.taxon = u.Species
+where c.rank="species"
+group by c.crop, u.country_code, u.year;
