1303f80567e333c52f849337fc3ee45fc9e202e4
Daniel Amariles <daniel.amariles88@gmail.com>
2019-07-04 09:27:48 -0500

    fixing MLS metric2

--- src/SQL/MLS_metric2.sql
+++ src/SQL/MLS_metric2.sql
@@ -2,29 +2,33 @@ use genesys_2018;
 
 
 create table CROP_INSTITUTION_GENUS_MLS_metric2 as 
-(select c.crop, g.institution_country, g.institution, 'N'as MLS_status, count(*) count
+(select c.crop, g.institution_country, g.institution, 'N' as MLS_status, count(*) count
 FROM GMERGE_uniques g
 left join CIAT_crop_taxon c on (g.genus=c.taxon)
 where c.rank="genus"
-group by c.crop, g.institution_country);
+group by c.crop, g.institution_country, g.institution);
 
+-- Query OK, 17130 rows affected (6 min 24.18 sec)
 
 update CROP_INSTITUTION_GENUS_MLS_metric2 m
 set MLS_status ='Y'
-where m.crop in (select cl.Common_name_standard from crop_species_list cl where cl.`MLS Annex 1`='Y' ) 
+where m.crop in (select cl.Common_name_standard from CROP_MLS cl where cl.`MLS Annex 1`='Y' ) 
 and m.institution in (select inst.instcode from INSTITUTION_MLS inst  where inst.MLS=1 ) ;
 
+-- Query OK, 4999 rows affected (4.74 sec)
+
+
 create table CROP_INSTITUTION_SPECIES_MLS_metric2 as 
-(select c.crop, g.institution_country, g.institution, 'N'as MLS_status, count(*) count
+(select c.crop, g.institution_country, g.institution, 'N' as MLS_status, count(*) count
 FROM GMERGE_uniques g
-left join CIAT_crop_taxon c on (g.genus=c.taxon)
+left join CIAT_crop_taxon c on (g.species=c.taxon)
 where c.rank="species"
-group by c.crop, g.institution_country);
+group by c.crop, g.institution_country, g.institution);
 
 
 update CROP_INSTITUTION_SPECIES_MLS_metric2 m
 set MLS_status ='Y'
-where m.crop in (select cl.Common_name_standard from crop_species_list cl where cl.`MLS Annex 1`='Y' ) 
+where m.crop in (select cl.Common_name_standard from CROP_MLS cl where cl.`MLS Annex 1`='Y' ) 
 and m.institution in (select inst.instcode from INSTITUTION_MLS inst  where inst.MLS=1 ) ;
 
 
