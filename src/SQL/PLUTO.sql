56c45e1aaffecfe54fdf2cfeeb267e980d5262a3
Daniel Amariles <daniel.amariles88@gmail.com>
2019-07-09 14:01:52 -0500

    extracting pluto counts

--- src/SQL/PLUTO.sql
+++ src/SQL/PLUTO.sql
@@ -16,3 +16,51 @@ order by count desc;
 SELECT uc, count(*) count FROM genesys_2018.PLUTO
 group by uc
 order by count desc;
+
+SELECT party_breeder FROM genesys_2018.PLUTO
+group by party_breeder;
+
+SELECT * FROM genesys_2018.PLUTO
+order by rand()
+limit 300;
+
+
+SELECT parties_text FROM genesys_2018.PLUTO
+group by parties_text;
+
+SELECT * FROM genesys_2018.PLUTO
+where cc="CO";
+
+select cleaned, count(*) from(
+SELECT SUBSTR(trim(lower(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+party_titleHolder,'`',''),'-',''),'/',''),';',''),' ',''),',',''),':',''),'1',''),'2',''),'3',''),'4',''),'5',''),'6',''),'7',''),'8',''),'9',''),'0',''),'&','and'),'\'',''),'"',''),'.',''))) 
+, 1, 20) 
+as cleaned, party_titleHolder
+
+ FROM genesys_2018.PLUTO  
+ group by party_titleHolder) m 
+ group by cleaned
+;
+
+
