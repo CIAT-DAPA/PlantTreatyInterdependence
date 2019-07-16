e1e1283002f1e36ac497d4a0a9d52bc447972c97
Daniel Amariles <daniel.amariles88@gmail.com>
2019-07-16 09:57:42 -0500

    saving Included and Not Included in MLS as different columns

--- src/SQL/MLS.sql
+++ src/SQL/MLS.sql
@@ -1,4 +1,4 @@
-create table CROP_INSTITUTION_GENUS_MLS as 
+create table CROP_INSTITUTION_GENUS_MLS_metric1 as 
 (select c.crop, g.institution_country, MLS_Status, count(*) count
 FROM GMERGE_uniques g
 left join CIAT_crop_taxon c on (g.genus=c.taxon)
@@ -6,11 +6,12 @@ where c.rank="genus"
 group by c.crop, g.institution_country, MLS_Status);
 
 
-create table CROP_INSTITUTION_SPECIES_MLS as 
+create table CROP_INSTITUTION_SPECIES_MLS_metric1 as 
 (select c.crop,  g.institution_country, MLS_Status, count(*) count
 FROM GMERGE_uniques g
 left join CIAT_crop_taxon c on (g.species=c.taxon)
 where c.rank="species"
 group by c.crop, g.institution_country, MLS_Status);
+
 
 
