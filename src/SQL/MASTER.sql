f37dd29d912bd939a71003ff71ecbabd3e5b409e
Daniel Amariles <daniel.amariles88@gmail.com>
2019-09-25 16:32:02 -0500

    update CGIAR codes

--- src/SQL/MASTER.sql
+++ src/SQL/MASTER.sql
@@ -5,7 +5,7 @@ use genesys_2018;
                                 
 
 -- GMERGE 
-create table GMERGE as (select     a.acceNumb as original_id, 
+DROP TABLE IF EXISTS GMERGE; create table GMERGE as (select     a.acceNumb as original_id, 
                                         LOWER(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(acceNumb, ';', ''), '_', ''), '\'', ''), '\"', ''), '@', ''), ':', ''), '.', ''), '-', ''), ' ', ''))  as id,
                                         "GENESYS" as source,
                                         instCode as institution,
@@ -187,7 +187,22 @@ WHERE institution_country = "" and source !="GBIF";
 
 
 -- International institutions
- UPDATE GMERGE SET institution_country = "" WHERE institution = "BEN089" OR institution = "CIV033" OR institution = "NER074" OR institution = "NER037" OR institution = "THA069" OR institution = "TWN001" OR institution = "TZA055" OR institution = "TWN012" OR institution = "TWN020" OR institution = "TWN016" OR institution = "UZB112" OR institution = "PHL598" OR institution = "THA128" OR institution = "TWN024" OR institution = "VNM126" OR institution = "VNM133" OR institution = "BGD069" OR institution = "BTN047" OR institution = "CMR147" OR institution = "ETH096" OR institution = "BEL084" OR institution = "BEN084" OR institution = "CMR195" OR institution = "ITA406" OR institution = "KEN207" OR institution = "TGO168" OR institution = "NPL095" OR institution = "TZA105" OR institution = "UGA332" OR institution = "GTM085" OR institution = "CRI001" OR institution = "CRI029" OR institution = "CRI026" OR institution = "CRI027" OR institution = "CRI085" OR institution = "CRI136" OR institution = "CRI134" OR institution = "CRI135" OR institution = "CRI142" OR institution = "CRI062" OR institution = "CRI033" OR institution = "HND020" OR institution = "PAN132" OR institution = "SLV092" OR institution = "BOL061" OR institution = "BOL009" OR institution = "BOL228" OR institution = "COL003" OR institution = "ETH038" OR institution = "ETH015" OR institution = "HND119" OR institution = "MWI034" OR institution = "ARG1306" OR institution = "BOL230" OR institution = "CUB546" OR institution = "KEN179" OR institution = "LAO073" OR institution = "PAN185" OR institution = "NIC036" OR institution = "PHL100" OR institution = "RWA011" OR institution = "THA126" OR institution = "TZA027" OR institution = "UGA034" OR institution = "PER334" OR institution = "TZA052" OR institution = "UGA139" OR institution = "ETH092" OR institution = "BFA035" OR institution = "BGD020" OR institution = "CRI063" OR institution = "ECU065" OR institution = "GHA026" OR institution = "IND346" OR institution = "KAZ008" OR institution = "KEN030" OR institution = "MEX129" OR institution = "MEX052" OR institution = "MEX002" OR institution = "MEX064" OR institution = "AUS172" OR institution = "BEN080" OR institution = "DZA079" OR institution = "ECU070" OR institution = "ECU069" OR institution = "UGA466" OR institution = "NGA044" OR institution = "NPL023" OR institution = "PHL094" OR institution = "PRY007" OR institution = "SYR035" OR institution = "THA065" OR institution = "TUR027" OR institution = "URY035" OR institution = "ZWE033" OR institution = "SLV062" OR institution = "SLV061" OR institution = "BOL038" OR institution = "MWI064" OR institution = "BDI007" OR institution = "BGD021" OR institution = "COL044" OR institution = "CYP018" OR institution = "ECU057" OR institution = "IDN065" OR institution = "IDN165" OR institution = "IND232" OR institution = "KEN031" OR institution = "MWI067" OR institution = "ARG1305" OR institution = "PAN183" OR institution = "PER863" OR institution = "PER001" OR institution = "PER721" OR institution = "PHL097" OR institution = "RWA039" OR institution = "RWA012" OR institution = "PER073" OR institution = "UGA252" OR institution = "VNM146" OR institution = "JOR090" OR institution = "EGY078" OR institution = "EGY217" OR institution = "LBN002" OR institution = "MEX126" OR institution = "AUS172" OR institution = "DZA052" OR institution = "OMN012" OR institution = "SYR035" OR institution = "SYR002" OR institution = "SYR043" OR institution = "UZB067" OR institution = "MLI219" OR institution = "CMR081" OR institution = "CMR080" OR institution = "IDN172" OR institution = "KEN056" OR institution = "KEN023" OR institution = "KEN028" OR institution = "KEN207" OR institution = "SEN039" OR institution = "TZA078" OR institution = "PER486" OR institution = "IND922" OR institution = "ETH097" OR institution = "BFA001" OR institution = "IND132" OR institution = "IND135" OR institution = "IND134" OR institution = "IND136" OR institution = "IND223" OR institution = "IND002" OR institution = "IND133" OR institution = "KEN032" OR institution = "MEX030" OR institution = "MLI072" OR institution = "MWI023" OR institution = "NER015" OR institution = "NER047" OR institution = "UGA480" OR institution = "NGA051" OR institution = "SDN019" OR institution = "SEN108" OR institution = "SEN107" OR institution = "USA659" OR institution = "ZWE034" OR institution = "TGO092" OR institution = "USA646" OR institution = "CMR154" OR institution = "BEN027" OR institution = "BEN016" OR institution = "BFA034" OR institution = "BFA009" OR institution = "CIV065" OR institution = "CMR023" OR institution = "CMR057" OR institution = "COD014" OR institution = "GBR055" OR institution = "GHA048" OR institution = "MWI065" OR institution = "TGO091" OR institution = "NGA042" OR institution = "NGA057" OR institution = "NGA039" OR institution = "NGA043" OR institution = "NGA081" OR institution = "RWA013" OR institution = "SLE008" OR institution = "UGA033" OR institution = "ZWE035" OR institution = "NGA119" OR institution = "ETH013" OR institution = "KEN095" OR institution = "BEN055" OR institution = "ETH098" OR institution = "IND1308" OR institution = "UZB108" OR institution = "LKA189" OR institution = "BGD022" OR institution = "IDN066" OR institution = "IND296" OR institution = "KHM005" OR institution = "MDG012" OR institution = "MMR008" OR institution = "MMR122" OR institution = "MMR057" OR institution = "IDN153" OR institution = "IND685" OR institution = "JPN089" OR institution = "LAO015" OR institution = "MYS167" OR institution = "NER092" OR institution = "UGA532" OR institution = "UGA173" OR institution = "NGA045" OR institution = "PHL001" OR institution = "PHL096" OR institution = "THA117" OR institution = "TZA028" OR institution = "VNM077";
+ UPDATE GMERGE SET institution_country = "" WHERE institution 
+ IN ("BEL084","COL003","ETH013","IND002","CIV033","KEN056","MEX002","NGA039","PER001","PHL001","SYR002","BEN089","CIV033","NER074","NER037","THA069","TWN001","TZA055","TWN012","TWN020","TWN016","UZB112",
+"PHL598" , "THA128","TWN024","VNM126","VNM133","BGD069","BTN047","CMR147","ETH096","BEL084","BEN084","CMR195","ITA406","KEN207","TGO168","NPL095",
+"TZA105","UGA332","GTM085","CRI001","CRI029","CRI026","CRI027","CRI085","CRI136","CRI134","CRI135","CRI142","CRI062","CRI033","HND020",
+"PAN132","SLV092","BOL061","BOL009","BOL228","COL003","ETH038","ETH015","HND119","MWI034","ARG1306","BOL230","CUB546","KEN179","LAO073",
+"PAN185","NIC036","PHL100","RWA011","THA126","TZA027","UGA034","PER334","TZA052","UGA139","ETH092","BFA035","BGD020","CRI063","ECU065",
+"GHA026","IND346","KAZ008","KEN030","MEX129","MEX052","MEX002","MEX064","AUS172","BEN080","DZA079","ECU070","ECU069","UGA466","NGA044",
+"NPL023","PHL094","PRY007","SYR035","THA065","TUR027","URY035","ZWE033","SLV062","SLV061","BOL038","MWI064","BDI007","BGD021","COL044",
+"CYP018","ECU057","IDN065","IDN165","IND232","KEN031","MWI067","ARG1305","PAN183","PER863","PER001","PER721","PHL097","RWA039","RWA012",
+"PER073","UGA252","VNM146","JOR090","EGY078","EGY217","LBN002","MEX126","AUS172","DZA052","OMN012","SYR035","SYR002","SYR043","UZB067",
+"MLI219","CMR081","CMR080","IDN172","KEN056","KEN023","KEN028","KEN207","SEN039","TZA078","PER486","IND922","ETH097","BFA001","IND132",
+"IND135","IND134","IND136","IND223","IND002","IND133","KEN032","MEX030","MLI072","MWI023","NER015","NER047","UGA480","NGA051","SDN019",
+"SEN108","SEN107","USA659","ZWE034","TGO092","USA646","CMR154","BEN027","BEN016","BFA034","BFA009","CIV065","CMR023","CMR057","COD014",
+"GBR055","GHA048","MWI065","TGO091","NGA042","NGA057","NGA039","NGA043","NGA081","RWA013","SLE008","UGA033","ZWE035","NGA119","ETH013",
+"KEN095","BEN055","ETH098","IND1308","UZB108","LKA189","BGD022","IDN066","IND296","KHM005","MDG012","MMR008","MMR122","MMR057","IDN153",
+"IND685","JPN089","LAO015","MYS167","NER092","UGA532","UGA173","NGA045","PHL001","PHL096","THA117","TZA028","VNM077");
 
 UPDATE GMERGE
 SET MLS_Status = "Not included"
@@ -204,16 +219,9 @@ WHERE MLS_Status is NULL;
 
 
 
--- duplicates
-#create table GMERGE_duplicates as select * from (
-#    select id, genus, count(*)  total
-#    from GMERGE
-#    group by id, genus
-#)j
-#where total > 1;
-
 -- uniques
-create table GMERGE_uniques as select * from (
+
+DROP TABLE IF EXISTS GMERGE_uniques; create table GMERGE_uniques as select * from (
     select * 
     from GMERGE
     group by id, genus
@@ -224,7 +232,7 @@ create table GMERGE_uniques as select * from (
 
 -- summary lite
 
-create table CROP_ORIGIN_GENUS_LITE as 
+DROP TABLE IF EXISTS CROP_ORIGIN_GENUS_LITE; create table CROP_ORIGIN_GENUS_LITE as 
 (select c.crop, g.orig_country, count(*) count
 FROM GMERGE_uniques g
 left join CIAT_crop_taxon c on (g.genus=c.taxon)
@@ -233,7 +241,7 @@ group by c.crop, g.orig_country);
 
 
 
-create table CROP_ORIGIN_SPECIES_LITE as 
+DROP TABLE IF EXISTS CROP_ORIGIN_SPECIES_LITE; create table CROP_ORIGIN_SPECIES_LITE as 
 (select c.crop, g.orig_country, count(*) count
 FROM GMERGE_uniques g
 left join CIAT_crop_taxon c on (g.species=c.taxon)
@@ -242,7 +250,7 @@ group by c.crop, g.orig_country);
 
 
 
-create table CROP_INSTITUTION_GENUS_LITE as 
+DROP TABLE IF EXISTS CROP_INSTITUTION_GENUS_LITE; create table CROP_INSTITUTION_GENUS_LITE as 
 (select c.crop, g.institution_country, count(*) count
 FROM GMERGE_uniques g
 left join CIAT_crop_taxon c on (g.genus=c.taxon)
@@ -251,7 +259,7 @@ group by c.crop, g.institution_country);
 
 
 
-create table GBIF_INSTITUTION_SPECIES_LITE as 
+DROP TABLE IF EXISTS GBIF_INSTITUTION_SPECIES_LITE; create table GBIF_INSTITUTION_SPECIES_LITE as 
 (select c.crop,  g.institution_country, count(*) count
 FROM GMERGE g
 left join CIAT_crop_taxon c on (g.species=c.taxon)
@@ -259,7 +267,7 @@ where c.rank="species"
 and g.source="GBIF"
 group by c.crop, g.institution_country);
 
-create table GBIF_INSTITUTION_GENUS_LITE as 
+DROP TABLE IF EXISTS GBIF_INSTITUTION_GENUS_LITE; create table GBIF_INSTITUTION_GENUS_LITE as 
 (select c.crop, g.institution_country, count(*) count
 FROM GMERGE g
 left join CIAT_crop_taxon c on (g.genus=c.taxon)
@@ -269,7 +277,7 @@ group by c.crop, g.institution_country);
 
 
 
-create table CROP_INSTITUTION_SPECIES_LITE as 
+DROP TABLE IF EXISTS CROP_INSTITUTION_SPECIES_LITE; create table CROP_INSTITUTION_SPECIES_LITE as 
 (select c.crop,  g.institution_country, count(*) count
 FROM GMERGE_uniques g
 left join CIAT_crop_taxon c on (g.species=c.taxon)
@@ -280,7 +288,7 @@ group by c.crop, g.institution_country);
 
 -- MLS metric 1 
 
-create table CROP_INSTITUTION_GENUS_MLS_metric1 as 
+DROP TABLE IF EXISTS CROP_INSTITUTION_GENUS_MLS_metric1; create table CROP_INSTITUTION_GENUS_MLS_metric1 as 
 (select c.crop, g.institution_country, MLS_Status, count(*) count
 FROM GMERGE_uniques g
 left join CIAT_crop_taxon c on (g.genus=c.taxon)
@@ -288,7 +296,7 @@ where c.rank="genus"
 group by c.crop, g.institution_country, MLS_Status);
 
 
-create table CROP_INSTITUTION_SPECIES_MLS_metric1 as 
+DROP TABLE IF EXISTS CROP_INSTITUTION_SPECIES_MLS_metric1; create table CROP_INSTITUTION_SPECIES_MLS_metric1 as 
 (select c.crop,  g.institution_country, MLS_Status, count(*) count
 FROM GMERGE_uniques g
 left join CIAT_crop_taxon c on (g.species=c.taxon)
@@ -298,7 +306,7 @@ group by c.crop, g.institution_country, MLS_Status);
 
 -- MLS metric 2
 
-create table CROP_INSTITUTION_GENUS_MLS_metric2 as 
+DROP TABLE IF EXISTS CROP_INSTITUTION_GENUS_MLS_metric2; create table CROP_INSTITUTION_GENUS_MLS_metric2 as 
 (select c.crop, g.institution_country, g.institution, 'N' as MLS_status, count(*) count
 FROM GMERGE_uniques g
 left join CIAT_crop_taxon c on (g.genus=c.taxon)
@@ -312,10 +320,29 @@ set MLS_status ='Y'
 where m.crop in (select cl.Common_name_standard from CROP_MLS cl where cl.`MLS Annex 1`='Y' ) 
 and m.institution in (select inst.instcode from INSTITUTION_MLS inst  where inst.MLS=1 ) ;
 
+update CROP_INSTITUTION_GENUS_MLS_metric2 m
+set MLS_status ='Y'
+where m.institution_country in ("BEL084","COL003","ETH013","IND002","CIV033","KEN056","MEX002","NGA039","PER001","PHL001","SYR002","BEN089","CIV033","NER074","NER037","THA069","TWN001","TZA055","TWN012","TWN020","TWN016","UZB112",
+"PHL598" , "THA128","TWN024","VNM126","VNM133","BGD069","BTN047","CMR147","ETH096","BEL084","BEN084","CMR195","ITA406","KEN207","TGO168","NPL095",
+"TZA105","UGA332","GTM085","CRI001","CRI029","CRI026","CRI027","CRI085","CRI136","CRI134","CRI135","CRI142","CRI062","CRI033","HND020",
+"PAN132","SLV092","BOL061","BOL009","BOL228","COL003","ETH038","ETH015","HND119","MWI034","ARG1306","BOL230","CUB546","KEN179","LAO073",
+"PAN185","NIC036","PHL100","RWA011","THA126","TZA027","UGA034","PER334","TZA052","UGA139","ETH092","BFA035","BGD020","CRI063","ECU065",
+"GHA026","IND346","KAZ008","KEN030","MEX129","MEX052","MEX002","MEX064","AUS172","BEN080","DZA079","ECU070","ECU069","UGA466","NGA044",
+"NPL023","PHL094","PRY007","SYR035","THA065","TUR027","URY035","ZWE033","SLV062","SLV061","BOL038","MWI064","BDI007","BGD021","COL044",
+"CYP018","ECU057","IDN065","IDN165","IND232","KEN031","MWI067","ARG1305","PAN183","PER863","PER001","PER721","PHL097","RWA039","RWA012",
+"PER073","UGA252","VNM146","JOR090","EGY078","EGY217","LBN002","MEX126","AUS172","DZA052","OMN012","SYR035","SYR002","SYR043","UZB067",
+"MLI219","CMR081","CMR080","IDN172","KEN056","KEN023","KEN028","KEN207","SEN039","TZA078","PER486","IND922","ETH097","BFA001","IND132",
+"IND135","IND134","IND136","IND223","IND002","IND133","KEN032","MEX030","MLI072","MWI023","NER015","NER047","UGA480","NGA051","SDN019",
+"SEN108","SEN107","USA659","ZWE034","TGO092","USA646","CMR154","BEN027","BEN016","BFA034","BFA009","CIV065","CMR023","CMR057","COD014",
+"GBR055","GHA048","MWI065","TGO091","NGA042","NGA057","NGA039","NGA043","NGA081","RWA013","SLE008","UGA033","ZWE035","NGA119","ETH013",
+"KEN095","BEN055","ETH098","IND1308","UZB108","LKA189","BGD022","IDN066","IND296","KHM005","MDG012","MMR008","MMR122","MMR057","IDN153",
+"IND685","JPN089","LAO015","MYS167","NER092","UGA532","UGA173","NGA045","PHL001","PHL096","THA117","TZA028","VNM077");
+
+
 -- Query OK, 4999 rows affected (4.74 sec)
 
 
-create table CROP_INSTITUTION_SPECIES_MLS_metric2 as 
+DROP TABLE IF EXISTS CROP_INSTITUTION_SPECIES_MLS_metric2; create table CROP_INSTITUTION_SPECIES_MLS_metric2 as 
 (select c.crop, g.institution_country, g.institution, 'N' as MLS_status, count(*) count
 FROM GMERGE_uniques g
 left join CIAT_crop_taxon c on (g.species=c.taxon)
@@ -328,12 +355,40 @@ set MLS_status ='Y'
 where m.crop in (select cl.Common_name_standard from CROP_MLS cl where cl.`MLS Annex 1`='Y' ) 
 and m.institution in (select inst.instcode from INSTITUTION_MLS inst  where inst.MLS=1 ) ;
 
+update CROP_INSTITUTION_SPECIES_MLS_metric2 m
+set MLS_status ='Y'
+where m.institution_country in ("BEL084","COL003","ETH013","IND002","CIV033","KEN056","MEX002","NGA039","PER001","PHL001","SYR002","BEN089","CIV033","NER074","NER037","THA069","TWN001","TZA055","TWN012","TWN020","TWN016","UZB112",
+"PHL598" , "THA128","TWN024","VNM126","VNM133","BGD069","BTN047","CMR147","ETH096","BEL084","BEN084","CMR195","ITA406","KEN207","TGO168","NPL095",
+"TZA105","UGA332","GTM085","CRI001","CRI029","CRI026","CRI027","CRI085","CRI136","CRI134","CRI135","CRI142","CRI062","CRI033","HND020",
+"PAN132","SLV092","BOL061","BOL009","BOL228","COL003","ETH038","ETH015","HND119","MWI034","ARG1306","BOL230","CUB546","KEN179","LAO073",
+"PAN185","NIC036","PHL100","RWA011","THA126","TZA027","UGA034","PER334","TZA052","UGA139","ETH092","BFA035","BGD020","CRI063","ECU065",
+"GHA026","IND346","KAZ008","KEN030","MEX129","MEX052","MEX002","MEX064","AUS172","BEN080","DZA079","ECU070","ECU069","UGA466","NGA044",
+"NPL023","PHL094","PRY007","SYR035","THA065","TUR027","URY035","ZWE033","SLV062","SLV061","BOL038","MWI064","BDI007","BGD021","COL044",
+"CYP018","ECU057","IDN065","IDN165","IND232","KEN031","MWI067","ARG1305","PAN183","PER863","PER001","PER721","PHL097","RWA039","RWA012",
+"PER073","UGA252","VNM146","JOR090","EGY078","EGY217","LBN002","MEX126","AUS172","DZA052","OMN012","SYR035","SYR002","SYR043","UZB067",
+"MLI219","CMR081","CMR080","IDN172","KEN056","KEN023","KEN028","KEN207","SEN039","TZA078","PER486","IND922","ETH097","BFA001","IND132",
+"IND135","IND134","IND136","IND223","IND002","IND133","KEN032","MEX030","MLI072","MWI023","NER015","NER047","UGA480","NGA051","SDN019",
+"SEN108","SEN107","USA659","ZWE034","TGO092","USA646","CMR154","BEN027","BEN016","BFA034","BFA009","CIV065","CMR023","CMR057","COD014",
+"GBR055","GHA048","MWI065","TGO091","NGA042","NGA057","NGA039","NGA043","NGA081","RWA013","SLE008","UGA033","ZWE035","NGA119","ETH013",
+"KEN095","BEN055","ETH098","IND1308","UZB108","LKA189","BGD022","IDN066","IND296","KHM005","MDG012","MMR008","MMR122","MMR057","IDN153",
+"IND685","JPN089","LAO015","MYS167","NER092","UGA532","UGA173","NGA045","PHL001","PHL096","THA117","TZA028","VNM077");
 
 
+DROP TABLE IF EXISTS CROP_PLUTO_SPECIES; create table CROP_PLUTO_SPECIES as (
+
+select c.crop as crop, 
+"World" as country,
+p.year as year,
+COUNT(*) as upov_species_varietal_release
+from PLUTO_CLEAN p
+left join CIAT_crop_taxon c on (p.species=c.taxon)
+where c.rank="species"
+ p.year IN ("2018","2017","2016","2015","2014")
+group by c.crop, p.year
+order by c.crop, p.year
 
 
-
-
+);
 
 
 
